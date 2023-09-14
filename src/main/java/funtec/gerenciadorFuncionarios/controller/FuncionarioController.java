7a155ffe2f41b631b570a89e7769daa97eeb262f
CaioHSilvaMC <caiohsilvamc@gmail.com>
2023-09-14 15:43:43 -0300

    Funções e Front-End

---
+++ src/main/java/funtec/gerenciadorFuncionarios/controller/FuncionarioController.java
@@ -0,0 +1,61 @@
+package funtec.gerenciadorFuncionarios.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import funtec.gerenciadorFuncionarios.model.Funcionario;
+import funtec.gerenciadorFuncionarios.model.reposiroty.FuncionarioReposiroty;
+import jakarta.validation.Valid;
+
+@Controller
+public class FuncionarioController {
+
+	@Autowired
+	FuncionarioReposiroty funcionarioRepository;
+	
+	@GetMapping ("/form")
+	public String funcionariosForm(Funcionario funcionario) { 
+		return "addFuncionarioForm";
+	}
+	
+	@PostMapping("/add")
+    public String novo(@Valid Funcionario funcionario, BindingResult result) {
+
+        if (result.hasFieldErrors()) {
+            return "redirect:/form";
+        }
+
+        funcionarioRepository.save(funcionario);
+
+        return "redirect:/home";
+    }
+	
+	@GetMapping ("form/{id}")
+	public String updateFuncionarioForm (Model model, @PathVariable (name = "id") int id) {
+		Funcionario funcionario = funcionarioRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));                 
+		model.addAttribute("funcionario", funcionario); 
+		return "atualizaForm";
+	}
+	
+	@PostMapping("update/{id}")     
+	public String alterarProduto(@Valid Funcionario funcionario, BindingResult result, @PathVariable int id) { 
+        if (result.hasErrors()) {             
+        	return "redirect:/form";         
+        }                  
+        
+        funcionarioRepository.save(funcionario);         
+        	return "redirect:/home";     
+    } 
+	
+	@GetMapping("delete/{id}")          
+	public String delete(@PathVariable(name = "id") int id, Model model) { 
+        Funcionario funcionario = funcionarioRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));                 
+        funcionarioRepository.delete(funcionario);         
+        return "redirect:/home"; 
+	}
+}
--- src/main/java/funtec/gerenciadorFuncionarios/model/reposiroty/FuncionarioReposiroty.java
+++ src/main/java/funtec/gerenciadorFuncionarios/model/reposiroty/FuncionarioReposiroty.java
@@ -3,13 +3,14 @@ package funtec.gerenciadorFuncionarios.model.reposiroty;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import funtec.gerenciadorFuncionarios.model.Funcionario;
 import funtec.gerenciadorFuncionarios.model.FuncionarioSetor;
 
-@Repository
+
 public interface FuncionarioReposiroty extends JpaRepository<Funcionario, Integer> {
 
-	List<Funcionario> findbySetor (FuncionarioSetor funcionarioSetor);
+	List<Funcionario> findBySetor (FuncionarioSetor setor);
+	
+	
 }
